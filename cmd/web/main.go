867053ee7cc6a85bf873ff13e081eaa89d9a38b5
Jorge <jorge.onate.2017@upb.edu.co>
2022-11-28 23:32:54 -0500

    Resevations and form working

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,6 +9,7 @@ import (
 
 	"github.com/JorgeOnate/goBookings/internal/config"
 	"github.com/JorgeOnate/goBookings/internal/handlers"
+	"github.com/JorgeOnate/goBookings/internal/models"
 	"github.com/JorgeOnate/goBookings/internal/render"
 	"github.com/alexedwards/scs/v2"
 )
@@ -19,6 +21,9 @@ var session *scs.SessionManager
 
 // main is the main function
 func main() {
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
 	// change this to true when in production
 	app.InProduction = false
 
